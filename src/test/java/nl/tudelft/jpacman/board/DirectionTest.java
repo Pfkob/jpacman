a5d02a6cca9b6219e9bfc1eaad8b4716e4efd312
Pfkob <bk.piangfa@gmail.com>
2024-09-16 22:05:35 +0700

    add new test case

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -18,5 +18,26 @@ public class DirectionTest {
     void testNorth() {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
+        assertThat(north.getDeltaX()).isEqualTo(0);
+    }
+    @Test
+    void testSouth() {
+        Direction south = Direction.valueOf("SOUTH");
+        assertThat(south.getDeltaY()).isEqualTo(1);
+        assertThat(south.getDeltaX()).isEqualTo(0);
+    }
+
+    @Test
+    void testEast() {
+        Direction east = Direction.valueOf("EAST");
+        assertThat(east.getDeltaY()).isEqualTo(0);
+        assertThat(east.getDeltaX()).isEqualTo(1);
+    }
+
+    @Test
+    void testWest() {
+        Direction west = Direction.valueOf("WEST");
+        assertThat(west.getDeltaY()).isEqualTo(0);
+        assertThat(west.getDeltaX()).isEqualTo(-1);
     }
 }
